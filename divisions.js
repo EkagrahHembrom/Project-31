36ebe692d9be43bf7bbda08f1d64c7fe69e5d4c8
EkagrahHembrom <70428010+EkagrahHembrom@users.noreply.github.com>
2021-07-02 20:29:30 +0530

    Add files via upload

---
+++ divisions.js
@@ -0,0 +1,18 @@
+class Divisions {
+    constructer(x,y,w,h){
+        var ptions = {
+
+            isStatic: true
+        }
+        this.body = Bodies.rectangle(x,y,w,h, options)
+        this.w w;
+        this.h = h;
+        World.add.add(world,)
+    }
+    display( {
+        var pos = this.body.position;
+        rectMode( CENTER);
+        fill("white");
+        rect(pos.x, pos.y, this.w, this.h);
+    })
+}
